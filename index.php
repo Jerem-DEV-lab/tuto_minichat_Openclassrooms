d04a653055b8d8a8ba1fcdcc4cb45abb6a9048be
Jeremy Guillemet <guillemet.jeremy087@gmail.com>
2020-05-06 19:11:48 +0200

    connexion à la bdd ok

--- index.php
+++ index.php
@@ -1,6 +1,22 @@
 <?php
     include ('./templates/base.php');
     include ('./templates/nav.php');
-
 ?>
 
+<form action="./minichat_post.php" method="POST">
+
+    <label for="pseudo">Votre pseudo :
+        <input type="text" name="pseudo" placeholder="Votre pseudo" />
+    </label>
+
+    <label for="password">Votre mot de passe :
+        <input type="password" name="password" placeholder="Votre mot de passe">
+    </label>
+
+    <input type="submit" value="Envoyer" />
+</form>
+
+<div class="container">
+        <p><?php ?></p>
+
+</div>
